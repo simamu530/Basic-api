4af60d180e1bed355f90324e2aeb38f458b53553
simamu530 <58772879+simamu530@users.noreply.github.com>
2021-04-11 00:50:48 +0900

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\TodosController;
 |
 */
 
+Route::apiResource('/todo', TodosController::class);
 Route::get('/todo', [TodosController::class, 'get']);
 Route::post('/todo', [TodosController::class, 'addTodos']);
 Route::delete('/todo', [TodosController::class, 'delete']);
