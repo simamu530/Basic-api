2a4e50357fb90425d3c521de78539362f5652f76
simamu530 <58772879+simamu530@users.noreply.github.com>
2021-04-11 17:45:12 +0900

    Update 2021_03_24_091226_create_todos_table.php

--- database/migrations/2021_03_24_091226_create_todos_table.php
+++ database/migrations/2021_03_24_091226_create_todos_table.php
@@ -15,7 +15,7 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->text('title');
             $table->timestamps();
         });
     }
